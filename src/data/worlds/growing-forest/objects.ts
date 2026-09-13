ebe2364b01e5ce52b67c715b8377612f12c16568
Sithum Manusha <appuhamysithum@gmail.com>
2026-09-13 16:47:17 +0530

    feat: register river-woodland-flower object

--- src/data/worlds/growing-forest/objects.ts
+++ src/data/worlds/growing-forest/objects.ts
@@ -45,4 +45,12 @@ export const forestObjects: WorldObject[] = [
       githubUsername: "DileepaPRA",
     },
   },
+  {
+    id: "river-woodland-flower",
+    asset: "/assets/worlds/growing-forest/woodland-flower.svg",
+    contributor: {
+      displayName: "Sithum Manusha",
+      githubUsername: "SithumManusha",
+    },
+  },
 ];
