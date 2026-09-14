78d40c9d476d299a8da67b205f337ff893e88594
Kavindya R.S <165242205+sewmini12@users.noreply.github.com>
2026-09-14 10:16:29 +0530

    feat: place red-mushroom in forest-03

--- src/data/worlds/growing-forest/placements.ts
+++ src/data/worlds/growing-forest/placements.ts
@@ -32,8 +32,8 @@ export const forestPlacements: ObjectPlacement[] = [
   {
     objectId: "red-mushroom",
     segmentId: "forest-03",
-    x: 45.0,
-    y: 55.0,
+    x: 20.0,
+    y: 80.0,
     scale: 1.0,
     rotation: 0,
   },
